d53e69226d7910ef6c2a9660ef9397cd326ce765
LaylaLindalva <138811845+LaylaLindalva@users.noreply.github.com>
2024-03-11 12:06:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
+console.log(url)
+
+
+// const urlBase = "http://makeup-api.herokuapp.com/api/v1/products.json"
+
+// const marca = ['Annabelle, beneficiar']
+
+// const loadProdutos = async () => {
+//     const res = await fetch(`${urlBase}/[json.sombra, json.batom]`)
+//     return res.json()
+// }
+
+// const retornaMarca = async () => {
+//     const res = await fetch(`${urlBase}?brand=${marca}`)
+//     return await res.json()
+// }
+
+// const Retorno = async () =>{
+//     const [prdutos, marca] = await Promise.all([
+//         loadProdutos(),
+//         retornaMarca(),
+//     ]);
+// console.log('Produtos: ', prdutos.results)
+// console.log('Marca: ', marca.results)
+// }
+
+// Retorno()
+
